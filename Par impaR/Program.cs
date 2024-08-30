8a28b1837a15d1efd663bc31269609efea6f24dc
Janordonez <Janordonez@uamv.edu.ni>
2024-08-29 22:36:46 -0600

    Final

--- Par impaR/Program.cs
+++ Par impaR/Program.cs
@@ -12,21 +12,24 @@ namespace paroimpar
         int contPar;
         int contImp;
 
-
-
         double numTotal = 0;
         double ParTotal = 0;
         float impTotal = 0;
         double sqPar;
         double sevenImp = 0;
 
+        int CantNum;
+
         char opc;
 
         static void Main(string[] args)
         {
             ParesoImpares paresoImpares = new ParesoImpares();
-            do {
 
+            Console.WriteLine("Digite la cantidad de numeros que desea digitar:");
+            paresoImpares.CantNum = int.Parse(Console.ReadLine());
+            
+            for(int i = 0; i < paresoImpares.CantNum; i++) { 
                 Console.WriteLine("\nDigite numeros enteros:");
                 paresoImpares.numeros = int.Parse(Console.ReadLine());
 
@@ -58,16 +61,20 @@ namespace paroimpar
                     paresoImpares.contImp++;
                     paresoImpares.impTotal += paresoImpares.numeros;
                 }
+            }
 
-                Console.WriteLine("\nDesea ingresar otro numero?: ");
-                paresoImpares.opc = Console.ReadKey().KeyChar;
 
-            } while (paresoImpares.opc == 'S'|| paresoImpares.opc == 's');
 
-            paresoImpares.sqPar = Math.Sqrt(paresoImpares.ParTotal);
+            paresoImpares.sqPar = Math.Sqrt(paresoImpares.ParTotal); //Calcular raiz 
             paresoImpares.sevenImp = paresoImpares.impTotal % 7;
 
-            Console.WriteLine($"La suma total es : {paresoImpares.numTotal}");
+            Console.WriteLine($"La cantidad de positivos son: {paresoImpares.contPos}");
+            Console.WriteLine($"La cantidad de negativos son : {paresoImpares.contNeg}");
+            Console.WriteLine($"La cantidad de ceros son: {paresoImpares.contCer}");
+            Console.WriteLine($"La cantidad de los pares son: {paresoImpares.contPar}");
+            Console.WriteLine($"La cantidad de impares son: {paresoImpares.contImp}");
+
+            Console.WriteLine($"\nLa suma total es : {paresoImpares.numTotal}");
             Console.WriteLine($"La suma total de los pares es : {paresoImpares.ParTotal}");
             Console.WriteLine($"La suma total de los impares es : {paresoImpares.impTotal}");
             Console.WriteLine($"La suma total de los pares con raiz cuadrada : {paresoImpares.sqPar}");
